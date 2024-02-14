dbc395aa0f3b5d32c0214057e4c1e72512e56ea9
Dagasfi <daniel.gaspar98@gmail.com>
2024-02-14 15:26:44 +0100

    [UPD]: Login page

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,17 @@
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var toggleButton = document.querySelector('.toggle-password');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleButton.textContent = '👁️';
+    } else {
+        passwordInput.type = 'password';
+        toggleButton.textContent = '👁️';
+    }
+}
+
 $('#login').on('click', function() {
     let user = $('#username')[0].value
     loginInfo.username = user
